ee323cbcff6dfd92f90ab556b6393f06ec7f1af5
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-05-28 15:53:29 +0200

    'ottimizzato' algoritmo controllo

--- js/bonus.js
+++ js/bonus.js
@@ -1,4 +1,5 @@
 // conto e stampo i multipli di 3 compresi tra 10 e 30 
 for (i = 12; i <= 30; i += 3) {
     document.getElementById('count').innerHTML += i + '<br>';
+    console.log(i)
 }
--- js/script.js
+++ js/script.js
@@ -4,15 +4,17 @@
 var numUtente = parseInt(Math.random() * 6) + 1;
 var numPc = parseInt(Math.random() * 6) + 1;
 
+// inizializzo la variabile in pomeriggio cosi da fare solo due if 
+var result = 'pareggio';
+
 // controllo chi ha il numero piu alto 
 if (numUtente > numPc) {
-    document.getElementById('risultato').innerHTML = 'Hai vinto';
+    result = 'hai vinto';
 } else if (numUtente < numPc) {
-    document.getElementById('risultato').innerHTML = 'Hai perso';
-} else {
-    document.getElementById('risultato').innerHTML = 'Pareggio';
+    result = 'hai perso' ;
 }
-// mostro i numeri che sono usciti 
+// mostro i numeri che sono usciti e il risultato
+document.getElementById('risultato').innerHTML = result;
 document.getElementById('n-utente').innerHTML = 'Questo è il tuo numero random : ' + numUtente;
 document.getElementById('n-pc').innerHTML = 'Questo è il numero random del pc: ' + numPc;
 
