23ee54a7709ca3b3f08df8479c63bd0ed009a4f4
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-05-28 15:17:37 +0200

    add numeri random da 1 a 6 interi

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 // Gioco dei dadi Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
-
-
-
+// genero numero random per il pc e per l'utente 
+var numUtente = parseInt(Math.random() * 6) + 1;
+var numPc = parseInt(Math.random() * 6) + 1;
 
 
 
